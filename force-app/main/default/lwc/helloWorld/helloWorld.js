72c852fe9a45875c7e950b2295a41ccfba34164f
Kamil <kamilacc97@gmail.com>
2022-09-05 09:57:57 +0200

    up

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,9 +2,7 @@ import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
   greeting = 'World';
   changeHandler(event) {
-    
     this.greeting = event.target.value;
-
   }
 }
 
